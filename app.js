64faaa33af166707f83848abeac1a2f458dde66e
Manish <mjpatel84@gmail.com>
2023-10-18 16:11:56 -0400

    adds refs to elements in script and adds container to html

---
+++ app.js
@@ -0,0 +1,12 @@
+const audio = document.getElementById("audio");
+
+const playBtn = document.getElementById("play");
+const prevBtn = document.getElementById("previous");
+const nextBtn = document.getElementById("next");
+
+const progressContainer = document.getElementById("progress-container");
+const progress = document.getElementById("progress-bar");
+
+const songTitle = document.getElementById("song-title");
+const songCover = document.getElementById("song-cover");
+const artist = document.getElementById("artist");
